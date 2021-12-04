6e39afa856cf3b71a4bcf41f89e2a03ed0981d47
praveesh91 <praveesh91@gmail.com>
2021-12-04 18:13:54 +0530

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TableComponent from "./Components/TableComponent";
+import "./index.scss";
+
+ReactDOM.render(
+  <div>
+    {" "}
+    <TableComponent />
+  </div>,
+  document.getElementById("root")
+);
